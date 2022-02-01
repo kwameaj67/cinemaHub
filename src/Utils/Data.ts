0353b747be93094790dcc0ecbf884f0a6cba2397
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-01 20:18:49 +0000

    static data

---
+++ src/Utils/Data.ts
@@ -0,0 +1,67 @@
+export interface PopularMovie {
+    id: number
+    title: string
+    language: string,
+    overview: string,
+    vote_average: number,
+    poster_path: string
+    release_date: string
+}
+
+
+export const data: PopularMovie[] = [
+    {
+        id: 1,
+        title: "Robin Hood",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    },
+    {
+        id: 2,
+        title: "Grace From Grass",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    },
+    {
+        id: 3,
+        title: "All Eyez On Me",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    },
+    {
+        id: 4,
+        title: "See Legacy",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    },
+    {
+        id: 5,
+        title: "Altitude",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    },
+    {
+        id: 6,
+        title: "Impossible",
+        language: "",
+        overview: "",
+        vote_average:1.2,
+        poster_path: "https://image.tmdb.org/t/p/w1280/keIxh0wPr2Ymj0Btjh4gW7JJ89e.jpg",
+        release_date: "23-34-2021"
+    }
+]
