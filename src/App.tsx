109e14fd327d63d14e9fc6e1f33e89edd28ad398
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-01 20:19:35 +0000

    modified app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Suspense} from 'react';
 import './App.css';
 
+// import MainRouter from './Router/routes'
+
+const MainRouter = React.lazy(()=> import('./Router/routes'))
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={
+        <div className="loader">
+            <h1>Cinema Hub</h1>
+        </div>
+      }>
+         <MainRouter/>
+      </Suspense>
     </div>
   );
 }
