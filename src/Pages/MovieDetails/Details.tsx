7ac2bcd3b45130de4090a262f8d9030a722477d6
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-17 09:57:25 +0000

    created new page

---
+++ src/Pages/MovieDetails/Details.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import './details.css'
+import axios from 'axios'
+
+
+const DetailsPage: FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DetailsPage
