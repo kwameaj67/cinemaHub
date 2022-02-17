0ec1fabcb56202da5edec66c4d2595333cf7b0c9
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-17 09:57:57 +0000

    add new page to routes

--- src/Router/routes.tsx
+++ src/Router/routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Route, Routes }from 'react-router-dom'
-import { HomePage } from '../Pages/index'
+import { Route, Routes } from 'react-router-dom'
+import { HomePage, MovieDetails } from '../Pages/index'
 
 const MainRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
         </Routes>
     )
 }
